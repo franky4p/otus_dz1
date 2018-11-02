9488beaa5c6469c993ec532cd69c1e4771fbe572
Pavel Khlebnikov <franky4p@yandex.ru>
2018-11-02 22:06:58 +0300

    Рефакторинг

--- log_analyzer.py
+++ log_analyzer.py
@@ -8,14 +8,13 @@
 #                     '"$http_user_agent" "$http_x_forwarded_for"
 #                     "$http_X_REQUEST_ID" "$http_X_RB_USER" '
 #                     '$request_time';
-
 import os
 import re
 import logging
 import gzip
 import itertools
 import argparse
-import sys
+import json
 from datetime import datetime, date
 from collections import Counter, defaultdict
 from string import Template
@@ -27,9 +26,54 @@ config = {
     "NAME_LOG": "myOtus.log",
     "ERROR_PERCENT": 10,
     "NAME_PATERN": 
-    "nginx-access-ui.log-[0-9]{4}(0[1-9]|1[012])(0[1-9]|1[0-9]|2[0-9]|3[01])"
+    "nginx-access-ui.log-[0-9]{4}(0[1-9]|1[012])(0[1-9]|1[0-9]|2[0-9]|3[01])(\.gz)"
 }
 
+def main(config):
+
+    logging.info("Program started")
+
+    config.update(parse_config())
+
+    rex = re.compile(config.get("NAME_PATERN"))
+
+    last_date, dir_entry_log = searchfiles(config.get("LOG_DIR"), rex)
+
+    if dir_entry_log is None:
+        logging.info("No logs to analyze")
+        return
+
+    log_lines = gen_cat(dir_entry_log)
+
+    tuples, error_percent = grep(log_lines)
+
+    if error_percent > config.get("ERROR_PERCENT"):
+        logging.error("Error threshold exceeded")
+        return
+
+    log = zip_tuples(tuples)
+
+    report_table = most_common_value(log, 'request', 'request_time', 
+    config.get("REPORT_SIZE"))
+    
+    successfully = generate_report(report_table, last_date, 
+    config.get("REPORT_DIR"))
+
+    if successfully:
+        logging.info("Report generation {} completed".format(last_date))
+    else:
+        logging.info("Could not generate report {} ".format(last_date))
+
+
+def zip_tuples(tuples):
+    colnames = ('remote_addr', 'remote_user', 'http_x_real_ip', 'time_local', 
+    'request', 'status', 'body_bytes_sent', 'http_referer', 'http_user_agent',
+    'http_x_forwarded_for', 'http_X_REQUEST_ID', 'http_X_RB_USER',
+    'request_time')
+
+    log = (dict(zip(colnames,t)) for t in tuples) 
+
+    return log   
 
 def grep(log_lines):
     """Разбирает строки по заданному формату и возвращает кортеж
@@ -40,7 +84,7 @@ def grep(log_lines):
 
     match_obj = (pattern.match(line) for line in log_lines)
 
-    #финт ушами, создаем 2 генератора из одного, 
+    #финт ушами, создаем 2 генератора из одного,
     # чтобы проверить количество строк с нарушением формата
     tuple_gen = itertools.tee(match_obj)
 
@@ -55,8 +99,6 @@ def percent_error(generator_log):
     """Считает полное количество строк и количество строк, не 
     подощедших под формат и возвращает % несоответствия  
     """
-    #больше 60% времени цп жрет эта функция 
-    #с этим нужно что-то делать
     
     all_str = 0
     error_str = 0
@@ -67,58 +109,11 @@ def percent_error(generator_log):
             error_str += 1
     
     if all_str > 0:
-        error_percent = error_str/all_str * 100
+        error_percent = error_str / all_str * 100
     else:
         error_percent = 0
 
     return error_percent
-
-
-def main(config):
-
-    name_log = config.get("NAME_LOG")
-    folder = config.get("LOG_DIR")
-    report_dir = config.get("REPORT_DIR")
-    pattern_name = config.get("NAME_PATERN")
-    most_com = int(config.get("REPORT_SIZE"))
-    border_error_percent = int(config.get("ERROR_PERCENT"))
-
-    logging.basicConfig(filename=name_log, level=logging.INFO,
-      format="%(asctime)s %(levelname).1s %(message)s",
-      datefmt="%Y.%m.%d %H:%M:%S")
-
-    logging.info("Program started")
-
-    rex = re.compile(pattern_name)
-
-    last_date, dir_entry_log = searchfiles(folder, rex)
-
-    if dir_entry_log is None:
-        logging.info("Логов нет")
-        return
-
-    log_lines = gen_cat(dir_entry_log)
-
-    tuples, error_percent = grep(log_lines)
-
-    if error_percent > border_error_percent:
-        logging.error("Превышен порог ошибок")
-        return
-
-    colnames = ('remote_addr','remote_user','http_x_real_ip','time_local', 
-    'request', 'status','body_bytes_sent','http_referer','http_user_agent',
-    'http_x_forwarded_for','http_X_REQUEST_ID','http_X_RB_USER','request_time')
-
-    log = (dict(zip(colnames,t)) for t in tuples)
-
-    report_table = most_common_value(log, 'request', 'request_time', most_com)
-    
-    successfully = generate_report(report_table, last_date, report_dir)
-
-    if successfully:
-        logging.info("Формирование отчета {} завершено".format(last_date))
-    else:
-        logging.info("Не удалось сформировать отчет {} ".format(last_date))
 
 
 def generate_report(report_table, last_date, report_dir):
@@ -127,8 +122,9 @@ def generate_report(report_table, last_date, report_dir):
     """
     name_report = os.path.join(report_dir, "report-{}.html".format(last_date))
 
+    #проверка на уже сформированный отчет
     if os.path.exists(name_report):
-        logging.info("Отчет за {} уже сформирован".format(last_date))
+        logging.info("Report {} already formed".format(last_date))
         return False
 
     with open("report.html", "rt", encoding="utf-8") as report:
@@ -141,7 +137,7 @@ def generate_report(report_table, last_date, report_dir):
         with open(name_report, "wt") as new_report:
             new_report.write(report_tmlt)
     else:
-        logging.exception("Не удалось получить шаблон отчета")
+        logging.exception("Failed to get report template")
         return False
 
     return True
@@ -175,7 +171,7 @@ def most_common_value(log, srch_key, sum_key, most_com):
             max_time_count[line[srch_key]] = request_time
 
         #здесь теряется почти все приемущества генератора
-        #т.к. собираются все значения request_time из файла в список
+        #т.к.  собираются все значения request_time из файла в список
         #и это печально
         mediana_dict[line[srch_key]].append(request_time)
             
@@ -186,15 +182,15 @@ def most_common_value(log, srch_key, sum_key, most_com):
     for el in most_common:
         count_req = count_request.get(el[0])
         max_time = max_time_count.get(el[0])
-        time_perc = el[1]/sum_request_time * 100
-        count_perc = count_req/sum_request * 100
-        time_avg = el[1]/count_req
+        time_perc = el[1] / sum_request_time * 100
+        count_perc = count_req / sum_request * 100
+        time_avg = el[1] / count_req
 
         sort_list = sorted(mediana_dict.get(el[0]))
         if len(sort_list) % 2 == 0:
             index_med = int(len(sort_list) / 2)
             #середина между центральными значениями
-            time_med = (sort_list[index_med] + sort_list[index_med-1]) / 2
+            time_med = (sort_list[index_med] + sort_list[index_med - 1]) / 2
         else: 
             #целая часть от деления будет серединой
             index_med = len(sort_list) // 2   
@@ -213,50 +209,52 @@ def most_common_value(log, srch_key, sum_key, most_com):
 def searchfiles(folder, rex):
     """По заданному формату rex в папке folder ищет самый
     последний исходя из названия файл plate или .gz 
-    используется os.scandir поэтому требуется python 3.6 <
     """
     date_now = datetime.now().date()
 
     last_file = None
     last_date = date(1980,1,1)
 
-    try:
-       sc = os.scandir(folder) 
-    except FileNotFoundError:
-        logging.exception("Не удалось выбрать лог для анализа")    
-    else:
-        with sc:
-            for element in sc:
-                if element.is_file():
-                    need_file = rex.search(element.name)
+    files = scan_files(folder, rex)
 
-                    if need_file:
-                        list_date_file = re.findall(r"\d{4}\d{2}\d{2}", element.name)
-                        date_file = datetime.strptime(list_date_file[0], "%Y%m%d").date()
+    for element in files:
+        list_date_file = re.findall(r"\d{4}\d{2}\d{2}", element.name)
+        date_file = datetime.strptime(list_date_file[0], "%Y%m%d").date()
 
-                        #не придумал регулярку для фильтра .bz2 и т.п.
-                        if (element.name.endswith(list_date_file[0]) or 
-                        element.name.endswith(".gz")):
-                            #если есть сегодняшний лог, берем его, 
-                            #иначе ищем самый последний    
-                            if date_file == date_now:
-                                last_file = element
-                                last_date = date_file
-                                break
-                            elif date_file >= last_date:
-                                last_file = element
-                                last_date = date_file
+        #если есть сегодняшний лог, берем его,
+        #иначе ищем самый последний
+        if date_file == date_now:
+            last_file = element
+            last_date = date_file
+            break
+        elif date_file >= last_date:
+            last_file = element
+            last_date = date_file
 
     return last_date, last_file
 
 
+def scan_files(folder, rex):
+
+    try:
+       sc = os.scandir(folder) 
+    except FileNotFoundError:
+        logging.exception("Failed to select log for analysis.")    
+    else:
+        for element in sc:
+            need_file = rex.search(element.name)
+
+            if element.is_file() and need_file:
+                yield element
+
+        sc.close()
+    
+
 def gen_cat(logfile):
     """Открывает файл и считывает из него строки 
     """
-    if logfile.name.endswith(".gz"):
-        log = gzip.open(logfile.path, "rt", encoding = "utf-8")
-    else:
-        log = open(logfile.path,"rt", encoding = "utf-8")
+    log_opener = gzip.open if logfile.name.endswith(".gz") else open
+    log = log_opener(logfile.path, "rt", encoding = "utf-8")
 
     for line in log:
         yield line    
@@ -264,31 +262,41 @@ def gen_cat(logfile):
     log.close()                   
           
 
-def createParser ():
+def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', type=argparse.FileType(mode='rt', 
-     encoding='utf-8'), default='Config.txt')
+     encoding='utf-8'), default='Config.json')
  
     return parser
 
-if __name__ == "__main__":
-    parser = createParser()
-    #в теории это должно работать, но на моей машине 
-    #sys.argv[1:] возвращает пустой список вместо параметров командной строки
-    #т.ч. всегда берется default='Config.txt'
-    args = parser.parse_args(sys.argv[1:])
+
+def parse_config():
+    parser = create_parser()
+    #в теории это должно работать, но не моей машине
+    #т.ч.  всегда берется default='Config.json'
+    args = parser.parse_args()
     
-    #не совсем понял, каким должен быть формат конфига,
-    #т.ч. сделал просто текстовый файл
-    text = args.config.read().split('\n')
+    text = args.config.read()
     
     file_conf = {}
-    for line in text:
-        key, value = line.split(':')
-        file_conf[key] = value.strip()
+
+    try:
+        file_conf = json.loads(text)
+    except:
+        logging.exception("Failed to parse config.") 
 
     args.config.close()
-    print(file_conf)
-    config.update(file_conf)
 
-    main(config)
+    return file_conf
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(filename=config.get("NAME_LOG"), level=logging.INFO,
+      format="%(asctime)s %(levelname).1s %(message)s",
+      datefmt="%Y.%m.%d %H:%M:%S")
+
+    try:
+        main(config)
+    except:
+        logging.exception("Unhandled exception")
